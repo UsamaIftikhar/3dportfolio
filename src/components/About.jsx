9ed7a7e4a57d250dc321d93ee25db928899e7b2e
Usama1100 <usamaiftikhar59@gmail.com>
2024-05-01 02:56:26 +0500

    react-tilt added again

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,4 @@
-// import Tilt from "react-tilt";
+import Tilt from "react-tilt";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { services } from "../constants";
@@ -8,7 +8,7 @@ import { SectionWrapper } from "../HigherOrderComponents";
 const ServiceCard = ({ index, title, icon }) => {
   return (
     <>
-      {/* <Tilt className="xs:w-[250px] w-full"> */}
+      <Tilt className="xs:w-[250px] w-full">
         <motion.div
           variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
           className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
@@ -23,7 +23,7 @@ const ServiceCard = ({ index, title, icon }) => {
             </h3>
           </div>
         </motion.div>
-      {/* </Tilt> */}
+      </Tilt>
     </>
   );
 };
